28a767c396e398d1ac45b1ff46acf2fca8849567
yessicaq <yessicaq@buffalo.edu>
2025-02-13 16:54:44 -0500

    I added the About me page and took justins css file and redid it!

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Settings from "./Component/Settings";
 import AboutUs from "./AboutUs";
 import AboutJustin from "./Team Pages/AboutJustin"
+import AboutYessica from "./Team Pages/AboutYessica";
 import HomePage from "./Component/HomePage";
 import Navbar from "./Component/Navbar";
 import Friends from "./Component/Friends";
@@ -90,6 +91,7 @@ function App() {
               <Route path="/settings" element={<Settings />} />
               <Route path="/AboutUs" element={<AboutUs />} />
               <Route path="/about_justin" element={<AboutJustin />} />
+              <Route path="/about_yessica" element={<AboutYessica />} />
               <Route
                 path="/"
                 element={
