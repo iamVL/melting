b1160f9dbe350cfe9bc0e3d8bc57eecf2bae5a3c
yessicaq <yessicaq@buffalo.edu>
2025-03-04 12:26:40 -0500

    Fixing the styling of the recipe pages and adding the user's ability to leave and view reviews for a specific recipe. I had an accidental / in the first line

--- src/Component/RecipeDetails.jsx
+++ src/Component/RecipeDetails.jsx
@@ -1,4 +1,4 @@
-\import React, { useEffect, useState , useRef} from "react";
+import React, { useEffect, useState , useRef} from "react";
 import {Link, useParams} from "react-router-dom";
 import "../RecipeDetails.css";
 
